780a30e787c6aaa54b4dfea4f72f76697ac5ad4e
Yuki F <fachry.yuki@gmail.com>
2021-12-26 21:22:48 +0700

    aplikasi pertama soal no 1 nodejs

---
+++ app.js
@@ -0,0 +1,7 @@
+let data = "{\n  \"title\": \"my todo\",\n  \"todos\": [\n    {\n      \"id\": 1,\n      \"title\": \"first todo\",\n      \"description\": \"pick up milk\",\n      \"completed\": true,\n      \"date\": 1389878466730\n    },\n    {\n      \"id\": 2,\n      \"title\": \"second todo\",\n      \"description\": \"learn backbone\",\n      \"completed\": false,\n      \"date_completed\": null\n    },\n    {\n      \"id\": 3,\n      \"title\": \"third todo\",\n      \"description\": \"go for a run\",\n      \"completed\": false,\n      \"date_completed\": null\n    }\n  ]\n}\n";
+let dataJson = JSON.parse(data);
+console.log("Yuki Fachriansyah")
+// console.log(data);
+// console.log(dataJson);
+// let title = dataJson.title;
+// console.log(title);
